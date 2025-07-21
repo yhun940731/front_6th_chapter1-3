5eb018e64d20818c6c688f4bf5d55267183ac864
최용훈 <yonghun@douzone.com>
2025-07-21 16:42:57 +0900

    Feat: useMemo 개발.

--- packages/lib/src/hooks/useMemo.ts
+++ packages/lib/src/hooks/useMemo.ts
@@ -1,8 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { DependencyList } from "react";
+
+import { useRef } from "./useRef";
 import { shallowEquals } from "../equals";
 
 export function useMemo<T>(factory: () => T, _deps: DependencyList, _equals = shallowEquals): T {
   // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+
+  const valueRef = useRef<T | undefined>(undefined);
+  const depsRef = useRef<DependencyList | undefined>(undefined);
+
+  // 첫 번째 렌더링이거나 의존성이 변경된 경우
+  if (depsRef.current === undefined || !_equals(depsRef.current, _deps)) {
+    depsRef.current = _deps;
+    valueRef.current = factory();
+  }
+
+  return valueRef.current as T;
 }
