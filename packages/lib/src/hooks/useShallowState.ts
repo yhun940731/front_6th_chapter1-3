e6ba204e32b35bc65fff8e6d95f1cb290f573e45
최용훈 <yonghun@douzone.com>
2025-07-22 17:41:54 +0900

    Feat: useShallowState 개발.

--- packages/lib/src/hooks/useShallowState.ts
+++ packages/lib/src/hooks/useShallowState.ts
@@ -1,7 +1,17 @@
 import { useState } from "react";
 import { shallowEquals } from "../equals";
+import { useCallback } from "./useCallback";
 
-export const useShallowState = <T>(initialValue: Parameters<typeof useState<T>>[0]) => {
+export const useShallowState = <T>(initialValue: T | (() => T)) => {
   // useState를 사용하여 상태를 관리하고, shallowEquals를 사용하여 상태 변경을 감지하는 훅을 구현합니다.
-  return useState(initialValue);
+  const [state, setState] = useState<T>(initialValue);
+
+  const setShallowState = useCallback((newValue: T) => {
+    // shallowEquals를 사용하여 이전 상태와 새로운 상태를 비교합니다.
+    if (!shallowEquals(state, newValue)) {
+      setState(newValue);
+    }
+  }, []);
+
+  return [state, setShallowState] as const;
 };
