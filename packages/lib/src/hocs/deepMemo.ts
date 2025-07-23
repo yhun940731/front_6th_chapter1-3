9ae8edb940ef95c477c51860a27b1329e67b14d2
최용훈 <yonghun@douzone.com>
2025-07-23 22:00:00 +0900

    Feat: memo, deepMemo 개발.

--- packages/lib/src/hocs/deepMemo.ts
+++ packages/lib/src/hocs/deepMemo.ts
@@ -1,5 +1,10 @@
 import type { FunctionComponent } from "react";
 
+import { memo } from "./memo";
+import { deepEquals } from "../equals";
+
 export function deepMemo<P extends object>(Component: FunctionComponent<P>) {
-  return Component;
+  // deepEquals 함수를 사용하여 props 비교
+  // 앞에서 만든 memo를 사용
+  return memo(Component, deepEquals);
 }
--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,21 @@
-import { type FunctionComponent } from "react";
+import { createElement, type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
 
+import { useRef } from "../hooks";
+
+// memo HOC는 컴포넌트의 props를 얕은 비교하여 불필요한 리렌더링을 방지합니다.
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  const MemoizedComponent: FunctionComponent<P> = (props) => {
+    const prevPropsRef = useRef<P | null>(null);
+    const prevComponentRef = useRef<React.FunctionComponentElement<P> | null>(null);
+
+    if (prevPropsRef.current === null || !equals(prevPropsRef.current, props)) {
+      prevPropsRef.current = props;
+      prevComponentRef.current = createElement(Component, props);
+    }
+
+    return prevComponentRef.current;
+  };
+
+  return MemoizedComponent;
 }
