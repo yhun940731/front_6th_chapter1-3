cc742deaaefa85096008c174ebd91b7dd68a5af2
최용훈 <yonghun@douzone.com>
2025-07-25 15:33:50 +0900

    Deploy: deploy

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,9 +1,18 @@
 import react from "@vitejs/plugin-react-oxc";
 import { createViteConfig } from "../../createViteConfig";
+import { resolve } from "path";
 
 const base: string = process.env.NODE_ENV === "production" ? "/front_6th_chapter1-3/" : "";
 
 export default createViteConfig({
   base,
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "404.html"),
+      },
+    },
+  },
 });
